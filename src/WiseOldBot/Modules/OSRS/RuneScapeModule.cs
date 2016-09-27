280436eae393a88120efdf641eda8af5d27f302e
AntiTcb <tcbskater@hotmail.com>
2016-09-27 18:43:44 -0400

    Implementations

--- src/WiseOldBot/Modules/OSRS/RuneScapeModule.cs
+++ src/WiseOldBot/Modules/OSRS/RuneScapeModule.cs
@@ -14,16 +14,87 @@
 namespace WiseOldBot.OSRS {
     #region Using
 
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using BCL;
+    using Discord;
     using Discord.Commands;
 
     #endregion
 
     [Module]
-    public class RuneScapeModule {
+    public partial class RuneScapeModule {
+        WiseOldBotConfig _config;
         #region Public Structs + Classes
 
-        [Group("stats")]
-        public partial class StatsGroup { }
+        public RuneScapeModule(WiseOldBotConfig config) {
+            _config = config;
+
+        }
+
+        [Command("defname"), Alias("account")]
+        public async Task ManageUsernameMapAsync(IUserMessage msg, [Remainder] string username)
+        {
+            var userID = msg.Author.Id;
+            if (!_config.UsernameMap.ContainsKey(userID))
+            {
+                _config.UsernameMap.Add(userID, null);
+            }
+            switch (username)
+            {
+                case "--unset":
+                    _config.UsernameMap[userID] = null;
+                    await msg.Channel.SendMessageAsync("Default account name unset!");
+                    break;
+                default:
+                    _config.UsernameMap[userID] = username;
+                    await msg.Channel.SendMessageAsync($"Default account name set to {Format.Code(_config.UsernameMap[userID])}");
+                    break;
+            }
+            ConfigHandler.Save(BotBase.CONFIG_PATH, _config);
+        }
+
+        [Command("lvl2exp"), Alias("l2e")]
+        public async Task CalculateLevelAsync(IUserMessage msg, uint level) {
+            var exp = LevelToExperience(level);
+
+            if (level > 99) {
+                await msg.Channel.SendMessageAsync($"Minimum exp: {exp}.");
+            }
+            else {
+                var nextLevelExp = LevelToExperience(level + 1);
+                await msg.Channel.SendMessageAsync($"Minimum exp: {exp}. Next level: {nextLevelExp}. Difference:{nextLevelExp - exp}");
+            }
+        }
+
+
+        [Command("exp2lvl"), Alias("e2l")]
+        public async Task CalculateExperienceAsync(IUserMessage msg, uint exp) => await msg.Channel.SendMessageAsync($"Level: {ExperienceToLevel(exp)}");
+
+        double LevelToExperience(uint level) {
+            double total = 0;
+            for (var i = 1 ; i < level ; i++) {
+                total += Math.Floor(i + 300 * Math.Pow(2, i / 7.0));
+            }
+            return Math.Floor(total / 4);
+        }
+
+        uint ExperienceToLevel(double exp) {
+            double pts = 0;
+            for (var lvl = 1; lvl < 99; ++lvl)
+            {
+                pts += Math.Floor(lvl + (300 * Math.Pow(2, lvl / 7D)));
+                var output = pts / 4D;
+                if (!(exp - output < 0)) {
+                    continue;
+                }
+                var l_out = (pts - Math.Floor(lvl + (300 * Math.Pow(2, lvl / 7D)))) / 4;
+                return (uint) (lvl + (exp - l_out) / (output - l_out));
+            }
+            return 99;
+        }
+
 
         #endregion Public Structs + Classes
     }
--- src/WiseOldBot/Modules/OSRS/StatsGroup.cs
+++ src/WiseOldBot/Modules/OSRS/StatsGroup.cs
@@ -6,7 +6,7 @@
 // Project: WiseOldBot
 // 
 // Created: 09/25/2016 6:29 AM
-// Last Revised: 09/25/2016 7:06 AM
+// Last Revised: 09/27/2016 7:09 AM
 // Last Revised by: Alex Gravely
 
 #endregion
@@ -16,162 +16,179 @@ namespace WiseOldBot.OSRS {
 
     using System;
     using System.Threading.Tasks;
-    using BCL.Preconditions;
     using Discord;
     using Discord.Commands;
     using RestEase;
 
     #endregion
 
-    public class StatsGroup {
-        #region Internal Fields + Properties
+    public partial class RuneScapeModule {
+        #region Public Structs + Classes
 
-        internal static IOSRSApi STATS_API = RestClient.For<IOSRSApi>(HS_ENDPOINT);
-        const string HS_ENDPOINT = "http://services.runescape.com/";
+        [Group("stats")]
+        public class StatsGroup {
+            #region Internal Fields + Properties
 
-        #endregion Internal Fields + Properties
+            internal static IOSRSApi STATS_API = RestClient.For<IOSRSApi>(HS_ENDPOINT);
+            const string HS_ENDPOINT = "http://services.runescape.com/";
+            WiseOldBotConfig _config;
 
-        #region Public Methods
+            #endregion Internal Fields + Properties
 
-        internal static async Task<Account> DownloadStatsAsync(string playerName, HighScoreType hsType) {
-            switch (hsType) {
-                case HighScoreType.Regular:
-                    return await STATS_API.GetRegularHighScoresAsync(playerName);
+            #region Public Methods
 
-                case HighScoreType.Ironman:
-                    return await STATS_API.GetIronmanHighScoresAsync(playerName);
+            public StatsGroup(WiseOldBotConfig config) {
+                _config = config;
+            }
 
-                case HighScoreType.Ultimate:
-                    return await STATS_API.GetUltimateHighScoresAsync(playerName);
+            internal static async Task<Account> DownloadStatsAsync(string playerName, HighScoreType hsType) {
+                switch (hsType) {
+                    case HighScoreType.Regular:
+                        return await STATS_API.GetRegularHighScoresAsync(playerName);
 
-                case HighScoreType.Deadman:
-                    return await STATS_API.GetDeadmanHighScoresAsync(playerName);
+                    case HighScoreType.Ironman:
+                        return await STATS_API.GetIronmanHighScoresAsync(playerName);
 
-                case HighScoreType.Seasonal:
-                    return await STATS_API.GetSeasonalHighScoresAsync(playerName);
+                    case HighScoreType.Ultimate:
+                        return await STATS_API.GetUltimateHighScoresAsync(playerName);
 
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(hsType), hsType, null);
+                    case HighScoreType.Deadman:
+                        return await STATS_API.GetDeadmanHighScoresAsync(playerName);
+
+                    case HighScoreType.Seasonal:
+                        return await STATS_API.GetSeasonalHighScoresAsync(playerName);
+
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(hsType), hsType, null);
+                }
+            }
+
+            #endregion Public Methods
+
+            [Command("stats")]
+            public async Task GetStatsAsync
+            (IUserMessage msg,
+             [Summary("High Score Type")] HighScoreType hsType = HighScoreType.Regular,
+             [Summary("Skill. Leave blank for all.")] SkillType skillType = SkillType.All,
+             [Remainder] string playerName = "") {
+                if (playerName == string.Empty) {
+                    playerName = _config.UsernameMap[msg.Author.Id];
+                }
+                var player = await DownloadStatsAsync(playerName, hsType);
+
+                switch (skillType) {
+                    case SkillType.Agility:
+                        await msg.Channel.SendMessageAsync(player.Agility.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Attack:
+                        await msg.Channel.SendMessageAsync(player.Attack.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Construction:
+                        await msg.Channel.SendMessageAsync(player.Construction.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Cooking:
+                        await msg.Channel.SendMessageAsync(player.Cooking.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Crafting:
+                        await msg.Channel.SendMessageAsync(player.Crafting.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Defense:
+                        await msg.Channel.SendMessageAsync(player.Defense.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Farming:
+                        await msg.Channel.SendMessageAsync(player.Farming.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Firemaking:
+                        await msg.Channel.SendMessageAsync(player.Firemaking.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Fishing:
+                        await msg.Channel.SendMessageAsync(player.Fishing.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Fletching:
+                        await msg.Channel.SendMessageAsync(player.Fletching.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Herblore:
+                        await msg.Channel.SendMessageAsync(player.Herblore.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Hitpoints:
+                        await msg.Channel.SendMessageAsync(player.Hitpoints.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Hunter:
+                        await msg.Channel.SendMessageAsync(player.Hunter.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Magic:
+                        await msg.Channel.SendMessageAsync(player.Magic.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Mining:
+                        await msg.Channel.SendMessageAsync(player.Mining.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Prayer:
+                        await msg.Channel.SendMessageAsync(player.Prayer.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Ranged:
+                        await msg.Channel.SendMessageAsync(player.Ranged.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Runecrafting:
+                        await msg.Channel.SendMessageAsync(player.Runecrafting.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Slayer:
+                        await msg.Channel.SendMessageAsync(player.Slayer.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Smithing:
+                        await msg.Channel.SendMessageAsync(player.Smithing.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Strength:
+                        await msg.Channel.SendMessageAsync(player.Strength.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Thieving:
+                        await msg.Channel.SendMessageAsync(player.Thieving.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Total:
+                        await msg.Channel.SendMessageAsync(player.Total.ToDiscordMessage());
+                        break;
+
+                    case SkillType.All:
+                        await msg.Channel.SendMessageAsync(player.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Woodcutting:
+                        await msg.Channel.SendMessageAsync(player.Woodcutting.ToDiscordMessage());
+                        break;
+
+                    case SkillType.Combat:
+                        break;
+
+                    case SkillType.NonCombat:
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(skillType), skillType, null);
+                }
             }
         }
 
-        #endregion Public Methods
-        
-        [Command("stats")]
-        public async Task GetAllStatsAsync
-            (IUserMessage msg, [Summary("High Score Type")]HighScoreType hsType, SkillType skillType, [Remainder] string playerName) {
-            var player = await DownloadStatsAsync(playerName, hsType);
-
-            switch (skillType) {
-                case SkillType.Agility:
-                    await msg.Channel.SendMessageAsync(player.Agility.ToDiscordMessage());
-                    break;
-
-                case SkillType.Attack:
-                    await msg.Channel.SendMessageAsync(player.Attack.ToDiscordMessage());
-                    break;
-
-                case SkillType.Construction:
-                    await msg.Channel.SendMessageAsync(player.Construction.ToDiscordMessage());
-                    break;
-
-                case SkillType.Cooking:
-                    await msg.Channel.SendMessageAsync(player.Cooking.ToDiscordMessage());
-                    break;
-
-                case SkillType.Crafting:
-                    await msg.Channel.SendMessageAsync(player.Crafting.ToDiscordMessage());
-                    break;
-
-                case SkillType.Defense:
-                    await msg.Channel.SendMessageAsync(player.Defense.ToDiscordMessage());
-                    break;
-
-                case SkillType.Farming:
-                    await msg.Channel.SendMessageAsync(player.Farming.ToDiscordMessage());
-                    break;
-
-                case SkillType.Firemaking:
-                    await msg.Channel.SendMessageAsync(player.Firemaking.ToDiscordMessage());
-                    break;
-
-                case SkillType.Fishing:
-                    await msg.Channel.SendMessageAsync(player.Fishing.ToDiscordMessage());
-                    break;
-
-                case SkillType.Fletching:
-                    await msg.Channel.SendMessageAsync(player.Fletching.ToDiscordMessage());
-                    break;
-
-                case SkillType.Herblore:
-                    await msg.Channel.SendMessageAsync(player.Herblore.ToDiscordMessage());
-                    break;
-
-                case SkillType.Hitpoints:
-                    await msg.Channel.SendMessageAsync(player.Hitpoints.ToDiscordMessage());
-                    break;
-
-                case SkillType.Hunter:
-                    await msg.Channel.SendMessageAsync(player.Hunter.ToDiscordMessage());
-                    break;
-
-                case SkillType.Magic:
-                    await msg.Channel.SendMessageAsync(player.Magic.ToDiscordMessage());
-                    break;
-
-                case SkillType.Mining:
-                    await msg.Channel.SendMessageAsync(player.Mining.ToDiscordMessage());
-                    break;
-
-                case SkillType.Prayer:
-                    await msg.Channel.SendMessageAsync(player.Prayer.ToDiscordMessage());
-                    break;
-
-                case SkillType.Ranged:
-                    await msg.Channel.SendMessageAsync(player.Ranged.ToDiscordMessage());
-                    break;
-
-                case SkillType.Runecrafting:
-                    await msg.Channel.SendMessageAsync(player.Runecrafting.ToDiscordMessage());
-                    break;
-
-                case SkillType.Slayer:
-                    await msg.Channel.SendMessageAsync(player.Slayer.ToDiscordMessage());
-                    break;
-
-                case SkillType.Smithing:
-                    await msg.Channel.SendMessageAsync(player.Smithing.ToDiscordMessage());
-                    break;
-
-                case SkillType.Strength:
-                    await msg.Channel.SendMessageAsync(player.Strength.ToDiscordMessage());
-                    break;
-
-                case SkillType.Thieving:
-                    await msg.Channel.SendMessageAsync(player.Thieving.ToDiscordMessage());
-                    break;
-
-                case SkillType.Total:
-                    await msg.Channel.SendMessageAsync(player.Total.ToDiscordMessage());
-                    break;
-
-                case SkillType.All:
-                    await msg.Channel.SendMessageAsync(player.ToDiscordMessage());
-                    break;
-
-                case SkillType.Woodcutting:
-                    await msg.Channel.SendMessageAsync(player.Woodcutting.ToDiscordMessage());
-                    break;
-
-                case SkillType.Combat:
-                    break;
-
-                case SkillType.NonCombat:
-                    break;
-
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(skillType), skillType, null);
-            }
-        }
+        #endregion Public Structs + Classes
     }
 }
