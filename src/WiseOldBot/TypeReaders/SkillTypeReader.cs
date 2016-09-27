787a74fe46d9d942428288b1fa6756ab34727796
AntiTcb <tcbskater@hotmail.com>
2016-09-26 23:38:37 -0400

    Naming changes

--- src/WiseOldBot/TypeReaders/SkillTypeReader.cs
+++ src/WiseOldBot/TypeReaders/SkillTypeReader.cs
@@ -21,86 +21,86 @@ namespace WiseOldBot.TypeReaders {
             switch (input.ToLower()) {
                 case "agility":
                 case "agi":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Agility));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Agility));
 
                 case "attack":
                 case "att":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Attack));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Attack));
 
                 case "construction":
                 case "con":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Construction));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Construction));
 
                 case "cooking":
                 case "cook":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Cooking));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Cooking));
 
                 case "crafting":
                 case "craft":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Crafting));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Crafting));
 
                 case "defense":
                 case "def":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Defense));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Defense));
 
                 case "farming":
                 case "farm":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Farming));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Farming));
 
                 case "firemaking":
                 case "fire":
                 case "fm":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Firemaking));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Firemaking));
 
                 case "fletching":
                 case "fletch":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Fletching));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Fletching));
 
                 case "herblore":
                 case "herb":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Herblore));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Herblore));
 
                 case "hitpoints":
                 case "hp":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Hitpoints));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Hitpoints));
 
                 case "hunter":
                 case "hunt":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Hunter));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Hunter));
 
                 case "magic":
                 case "mage":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Magic));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Magic));
 
                 case "mining":
                 case "mine":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Mining));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Mining));
 
                 case "prayer":
                 case "pray":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Prayer));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Prayer));
 
                 case "ranged":
                 case "range":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Ranged));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Ranged));
 
                 case "runecrafting":
                 case "rc":
                 case "rune":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Runecrafting));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Runecrafting));
 
                 case "slayer":
                 case "slay":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Slayer));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Slayer));
 
                 case "smithing":
                 case "smith":
                 case "sm":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Smithing));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Smithing));
 
                 case "strength":
                 case "str":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Strength));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Strength));
 
                 case "thieving":
                 case "theiving":
@@ -109,27 +109,27 @@ namespace WiseOldBot.TypeReaders {
                 case "theiv":
                 case "t":
                 case "th":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Thieving));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Thieving));
 
                 case "woodcutting":
                 case "wood":
                 case "wc":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Woodcutting));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Woodcutting));
 
                 case "total":
                 case "all":
                 case "":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.All));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.All));
 
                 case "cb":
                 case "combatlevel":
                 case "combat":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.Combat));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.Combat));
 
                 case "noncombat":
                 case "ncm":
                 case "skiller":
-                    return Task.FromResult(TypeReaderResult.FromSuccess(Skill.NonCombat));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(SkillType.NonCombat));
 
                 default:
                     return Task.FromResult(TypeReaderResult.FromError(CommandError.ParseFailed, "Could not parse skill declaration."));
