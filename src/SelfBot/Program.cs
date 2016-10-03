8391e78256a767caba4f3d525aeced4f4b8b8bfe
AntiTcb <tcbskater@hotmail.com>
2016-10-03 15:19:14 -0400

    SelfBot

---
+++ src/SelfBot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SelfBot {
+    using BCL;
+    using Discord;
+    using Discord.Commands;
+    using Discord.WebSocket;
+
+    public class Program : BotBase {
+        public static void Main(string[] args) => new Program().StartAsync().GetAwaiter().GetResult();
+
+        #region Overrides of BotBase
+
+        public async override Task StartAsync() {
+            Client = new DiscordSocketClient(new DiscordSocketConfig { LogLevel = LogSeverity.Debug });
+            HandleConfigs();
+            await InstallCommandsAsync();
+            await LoginAndConnectAsync();
+        }
+
+        public override void HandleConfigs() => Configs = ConfigHandler.Load<SelfConfig>(CONFIG_PATH);
+
+        public async override Task InstallCommandsAsync() {
+            Commands = new CommandHandler();
+            var map = new DependencyMap();
+            map.Add(Configs);
+            await Commands.Install(Client, Configs, map);
+        }
+
+        #endregion
+    }
+}
---
+++ src/SelfBot/SelfConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SelfBot
+{
+    using BCL.Interfaces;
+    using Newtonsoft.Json;
+
+    public class SelfConfig : IConfig
+    {
+        #region Implementation of IConfig
+        [JsonProperty("botToken")]
+        public string BotToken { get; set; }
+        [JsonProperty("prefix")]
+        public char CommandPrefix { get; set; }
+
+        #endregion
+    }
+}
