fc828e9da027902e09a94da0e926c1d0efc630ff
AntiTcb <tcbskater@hotmail.com>
2016-10-16 23:04:16 -0400

    Updates

---
+++ src/SelfBot/SelfBotConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SelfBot
+{
+    using BCL.Interfaces;
+    using Newtonsoft.Json;
+
+    public class SelfBotConfig : IBotConfig
+    {
+        #region Implementation of IBotConfig
+        [JsonProperty("botToken")]
+        public string BotToken { get; set; }
+
+        [JsonProperty("prefix")]
+        public char CommandPrefix { get; set; } = ';';
+
+        public ulong LogChannel { get; set; }
+
+        [JsonProperty("evalImports")]
+        public HashSet<string> EvalImports { get; set; } = new HashSet<string>();
+
+        #endregion
+    }
+}
