5c42bd3b47d574b57614b3dfaa34022e365de241
AntiTcb <tcbskater@hotmail.com>
2016-10-21 03:52:56 -0400

    OrgBot updates

--- src/OrgBot/OrgBotConfig.cs
+++ src/OrgBot/OrgBotConfig.cs
@@ -19,10 +19,13 @@ namespace OrgBot {
         public string BotToken { get; set; }
         [JsonProperty("logChannel")]
         public ulong LogChannel { get; set; }
+        [JsonProperty("dbToken")]
+        public string DatabaseToken { get; set; }
 
-        public OrgBotConfig(string botToken, ulong logChannel) {
+        public OrgBotConfig(string botToken, ulong logChannel, string dbToken) {
             BotToken = botToken;
             LogChannel = logChannel;
+            DatabaseToken = dbToken;
         }
 
         #endregion
