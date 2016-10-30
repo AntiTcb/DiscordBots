ed7885d82415544162331c309b5f4b32c6cc1ea4
AntiTcb <tcbskater@hotmail.com>
2016-10-30 14:36:03 -0400

    Pulls Owner ID from discord api

--- src/BCL/Preconditions/RequireOwnerAttribute.cs
+++ src/BCL/Preconditions/RequireOwnerAttribute.cs
@@ -6,7 +6,7 @@
 // Project: BCL
 // 
 // Created: 09/26/2016 11:16 PM
-// Last Revised: 10/13/2016 7:32 PM
+// Last Revised: 10/25/2016 4:17 PM
 // Last Revised by: Alex Gravely
 
 #endregion
@@ -16,6 +16,7 @@ namespace BCL.Preconditions {
 
     using System;
     using System.Threading.Tasks;
+    using Discord;
     using Discord.Commands;
 
     #endregion
@@ -24,11 +25,16 @@ namespace BCL.Preconditions {
     public class RequireOwnerAttribute : PreconditionAttribute {
         #region Overrides of PreconditionAttribute
 
-        public override Task<PreconditionResult> CheckPermissions
-            (CommandContext context, CommandInfo command, IDependencyMap map) {
-            return Task.FromResult
-                (context.User.Id == Globals.OWNER_ID
-                     ? PreconditionResult.FromSuccess() : PreconditionResult.FromError("You are not the bot owner."));
+        IApplication _appInfo;
+
+        public async override Task<PreconditionResult> CheckPermissions(CommandContext ctx, CommandInfo cmd, IDependencyMap map) {
+            if (_appInfo == null) {
+                _appInfo = await ctx.Client.GetApplicationInfoAsync().ConfigureAwait(false);
+            }
+            return await Task.FromResult(
+                ctx.User.Id == _appInfo.Owner.Id
+                ? PreconditionResult.FromSuccess()
+                : PreconditionResult.FromError("You are not the bot owner.")).ConfigureAwait(false);
         }
 
         #endregion Overrides of PreconditionAttribute
