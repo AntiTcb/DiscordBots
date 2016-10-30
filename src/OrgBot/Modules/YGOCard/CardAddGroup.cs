8ddebeb684c6c0a35e2429dd896bcecdcfffa53a
AntiTcb <tcbskater@hotmail.com>
2016-10-30 14:38:12 -0400

    Init commit for adding cards

---
+++ src/OrgBot/Modules/YGOCard/CardAddGroup.cs
@@ -0,0 +1,25 @@
+﻿#region Header
+// Description:
+// 
+// Solution: DiscordBots
+// Project: OrgBot
+// 
+// Created: 10/23/2016 11:01 PM
+// Last Revised: 10/23/2016 11:01 PM
+// Last Revised by: Alex Gravely
+#endregion
+namespace OrgBot.Modules.YGOCard {
+    using System.Collections.Generic;
+    using Discord.Commands;
+    using Entities;
+
+    public partial class YGOCardModule {
+        public class CardAddGroup : ModuleBase {
+            //static Dictionary<IUser, YGOCard> NewCards { get; set; } -- Collection TBD
+            //public async Task StartCreateCardAsync() { }
+            //public async Task SaveCreateCardAsync() { }
+            //public async Task DeleteCreateCardASync() { }
+            //public asyn Task SetCardDetailsAsync() { } -- Broken into separate commands for each one, akin to edit?
+        }
+    }
+}
