4158f14ebde04ac53c8fb1c0c819e972d64e0c3c
AntiTcb <tcbskater@hotmail.com>
2016-10-30 14:39:47 -0400

    YugiohPricesModule init commit

---
+++ src/OrgBot/Modules/YugiohPrices/YugiohPricesModule.cs
@@ -0,0 +1,16 @@
+﻿#region Header
+// Description:
+// 
+// Solution: DiscordBots
+// Project: OrgBot
+// 
+// Created: 10/30/2016 2:08 PM
+// Last Revised: 10/30/2016 2:08 PM
+// Last Revised by: Alex Gravely
+#endregion
+namespace OrgBot.Modules.YugiohPrices {
+    using Discord.Commands;
+    public class YugiohPricesModule : ModuleBase {
+        
+    }
+}
