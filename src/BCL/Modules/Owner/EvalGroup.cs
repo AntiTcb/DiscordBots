64ffc0218fe0fa4cd9be2bc25e5cc215468f65a2
AntiTcb <tcbskater@hotmail.com>
2016-11-04 14:05:36 -0400

    Finished Eval implementation

--- src/BCL/Modules/Owner/EvalGroup.cs
+++ src/BCL/Modules/Owner/EvalGroup.cs
@@ -6,7 +6,7 @@
 // Project: BCL
 // 
 // Created: 10/27/2016 10:48 PM
-// Last Revised: 10/27/2016 10:54 PM
+// Last Revised: 11/03/2016 9:19 PM
 // Last Revised by: Alex Gravely
 
 #endregion
@@ -14,25 +14,17 @@
 namespace BCL.Modules.Owner {
     #region Using
 
-    using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Reflection;
     using System.Threading.Tasks;
-    using Discord;
     using Discord.Commands;
-    using Discord.WebSocket;
     using Interfaces;
-    using Microsoft.CodeAnalysis.CSharp.Scripting;
-    using Microsoft.CodeAnalysis.Scripting;
 
     #endregion
 
     public partial class OwnerModule {
         #region Public Structs + Classes
 
-        public sealed class EvalGlobals
-        {
+        public sealed class EvalGlobals {
             #region Internal Fields + Properties
 
             internal IBotConfig BC { get; set; }
@@ -44,57 +36,28 @@ namespace BCL.Modules.Owner {
 
         [Group("eval")]
         public class EvalGroup : ModuleBase {
-            #region Public Structs + Classes
-
-            [Group("imports")]
-            public class ImportGroup : ModuleBase {
-                #region Public Methods
-
-                [Command("add")]
-                public async Task AddImportAsync(string import) {
-                    Globals.EvalImports.Add(import);
-                    await ReplyAsync($"Added {import}").ConfigureAwait(false);
-                }
-
-                [Command("list"), Alias("l")]
-                public async Task ListImportsAsync()
-                    => await ReplyAsync(string.Join(", ", Globals.EvalImports.Select(x => x))).ConfigureAwait(false);
-
-                [Command("remove"), Alias("delete")]
-                public async Task RemoveImportAsync(string import)
-                {
-                    Globals.EvalImports.Remove(import);
-                    await ReplyAsync($"Removed {import}").ConfigureAwait(false);
-                }
-                #endregion Public Methods
-            }
-
-            #endregion Public Structs + Classes     
-
             #region Public Methods
+
+            [Command("add")]
+            public async Task AddImportAsync(string import) {
+                Globals.EvalImports.Add(import);
+                await ReplyAsync($"Added {import}").ConfigureAwait(false);
+            }
 
             [Command("execute"), Alias("run", "exec", "=>")]
             public async Task EvaluateAsync([Remainder] string expression) {
-                await Context.Channel.TriggerTypingAsync().ConfigureAwait(false);
-                var options =
-                    ScriptOptions.Default.AddReferences
-                                  (typeof(object).GetTypeInfo().Assembly.Location,
-                                   typeof(Enumerable).GetTypeInfo().Assembly.Location,
-                                   typeof(IUser).GetTypeInfo().Assembly.Location,
-                                   typeof(DiscordSocketClient).GetTypeInfo().Assembly.Location,
-                                   typeof(CommandContext).GetTypeInfo().Assembly.Location,
-                                   typeof(Assembly).GetTypeInfo().Assembly.Location).AddImports(Globals.EvalImports);
+                await EvalService.EvaluateAsync(Context, expression).ConfigureAwait(false);
+            }
 
-                var guildID = Context.Guild?.Id;
-                var globalObjects = guildID != null
-                                        ? new EvalGlobals {
-                                            CC = Context,
-                                            BC = Globals.BotConfig,
-                                            SC = Globals.ServerConfigs[guildID.Value]
-                                        }
-                                        : new EvalGlobals { CC = Context, BC = Globals.BotConfig };
-                var result = await CSharpScript.EvaluateAsync(expression, globals: globalObjects, globalsType: typeof(EvalGlobals)).ConfigureAwait(false);
-                await ReplyAsync($"Eval: {result}").ConfigureAwait(false);
+            [Command("list"), Alias("l")]
+            public async Task ListImportsAsync() {
+                await ReplyAsync(string.Join(", ", Globals.EvalImports.Select(x => x))).ConfigureAwait(false);
+            }
+
+            [Command("remove"), Alias("delete")]
+            public async Task RemoveImportAsync(string import) {
+                Globals.EvalImports.Remove(import);
+                await ReplyAsync($"Removed {import}").ConfigureAwait(false);
             }
 
             #endregion Public Methods
---
+++ src/BCL/Modules/Owner/EvalService.cs
@@ -0,0 +1,88 @@
+﻿#region Header
+
+// Description:
+// 
+// Solution: DiscordBots
+// Project: BCL
+// 
+// Created: 11/03/2016 8:33 PM
+// Last Revised: 11/03/2016 9:32 PM
+// Last Revised by: Alex Gravely
+
+#endregion
+
+namespace BCL.Modules.Owner {
+    #region Using
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using Discord.Commands;
+    using Discord.WebSocket;
+    using Microsoft.CodeAnalysis.CSharp.Scripting;
+    using Microsoft.CodeAnalysis.Scripting;
+
+    #endregion
+
+    public static class EvalService {
+        #region Private Fields + Properties
+
+        static readonly ScriptOptions _options;
+
+        #endregion Private Fields + Properties
+
+        #region Public Constructors
+
+        static EvalService() {
+            _options = ScriptOptions.Default.AddReferences(GetAssemblies().ToArray()).AddImports(Globals.EvalImports);
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        public static async Task EvaluateAsync(CommandContext context, string script) {
+            using (context.Channel.EnterTypingState()) {
+                var working = await context.Channel.SendMessageAsync("**Evaluating**, just a sec...");
+                var _globals = new ScriptGlobals { client = context.Client as DiscordSocketClient, context = context };
+                script = script.Trim('`');
+                try {
+                    var eval = await CSharpScript.EvaluateAsync(script, _options, _globals, typeof(ScriptGlobals));
+                    await context.Channel.SendMessageAsync(eval.ToString());
+                }
+                catch (Exception e) {
+                    await context.Channel.SendMessageAsync($"**Script Failed**\n{e.Message}");
+                }
+                finally {
+                    await working.DeleteAsync();
+                }
+            }
+        }
+
+        public static IEnumerable<Assembly> GetAssemblies() {
+            var Assemblies = Assembly.GetEntryAssembly().GetReferencedAssemblies();
+            foreach (var a in Assemblies) {
+                var asm = Assembly.Load(a);
+                yield return asm;
+            }
+            yield return Assembly.GetEntryAssembly();
+            yield return typeof(ILookup<string, string>).GetTypeInfo().Assembly;
+        }
+
+        #endregion Public Methods
+    }
+
+    public class ScriptGlobals {
+        #region Public Fields + Properties
+
+        public SocketGuildChannel channel => context.Channel as SocketGuildChannel;
+        public DiscordSocketClient client { get; internal set; }
+        public CommandContext context { get; internal set; }
+        public SocketGuild guild => context.Guild as SocketGuild;
+        public SocketMessage msg => context.Message as SocketMessage;
+
+        #endregion Public Fields + Properties
+    }
+}
