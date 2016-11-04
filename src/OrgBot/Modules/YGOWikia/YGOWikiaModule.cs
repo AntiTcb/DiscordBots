5194c7bda81dcf1e55ae92c6bdc1303c3d09d564
AntiTcb <tcbskater@hotmail.com>
2016-11-04 14:16:04 -0400

    Null checking, error handling.

--- src/OrgBot/Modules/YGOWikia/YGOWikiaModule.cs
+++ src/OrgBot/Modules/YGOWikia/YGOWikiaModule.cs
@@ -1,26 +1,41 @@
 ﻿#region Header
+
 // Description:
 // 
 // Solution: DiscordBots
 // Project: OrgBot
 // 
 // Created: 10/29/2016 6:36 PM
-// Last Revised: 10/29/2016 6:36 PM
+// Last Revised: 11/04/2016 12:49 AM
 // Last Revised by: Alex Gravely
+
 #endregion
+
 namespace OrgBot.Modules.YGOWikia {
+    #region Using
+
     using System.Threading.Tasks;
-    using BCL.Preconditions;
     using Discord.Commands;
     using YGOCard.Entities;
 
+    #endregion
+
     [Group("wikia")]
-    public partial class YGOWikiaModule : ModuleBase{
+    public class YGOWikiaModule : ModuleBase {
+        #region Public Methods
+
         [Command("card"), Alias("c")]
         public async Task GetCardAsync([Remainder] string cardName) {
-            await Context.Channel.TriggerTypingAsync();
-            var card = await YGOWikiaClient.GetCardAsync(cardName);
-            await ReplyAsync(card?.ToDiscordMessage() ?? "Invalid card name/Card not found");
+            using (Context.Channel.EnterTypingState()) {
+                var card = await YGOWikiaClient.GetCardAsync(cardName);
+                if (card?.Name == "Parse failed.") {
+                    await ReplyAsync("Parsing of card failed.");
+                    return;
+                }
+                await ReplyAsync(card?.ToDiscordMessage() ?? "Invalid card name/Card not found.");
+            }
         }
+
+        #endregion Public Methods
     }
 }
