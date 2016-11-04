5e217e27dfe97d792e7cb4c3a3a9db5ada795090
AntiTcb <tcbskater@hotmail.com>
2016-11-04 14:13:59 -0400

    Refactors, bugfixes.

--- src/OrgBot/Modules/YGOCard/YGOCardModule.cs
+++ src/OrgBot/Modules/YGOCard/YGOCardModule.cs
@@ -47,14 +47,16 @@ namespace OrgBot.Modules.YGOCard {
                 await ReplyAsync("I need a card name!");
                 return;
             }
-            var card = YGOCardAPIClient.Cards.FindCards(cardName.ToLower()).FirstOrDefault();
+            var card = YGOCardAPIClient.Cards.FindCards(cardName).FirstOrDefault();
             if (card == null) {
                 await ReplyAsync("Card not found.");
                 return;
             }
             if (card.Name == "Spirit Elimination") {
-                await ReplyAsync("NO! NO, NO, NO, NO, NO! NO! STOP ASKING QUESTIONS ABOUT THIS CARD!");
+                var msg = await ReplyAsync("NO! NO, NO, NO, NO, NO! NO! STOP ASKING QUESTIONS ABOUT THIS CARD!");
                 await Task.Delay(4000);
+                await msg.ModifyAsync(x => x.Content = card.ToDiscordMessage());
+                return;
             }
             if (card.Name.Contains("Bujin")) {
                 await ReplyAsync("Bujin master race!");
@@ -74,10 +76,14 @@ namespace OrgBot.Modules.YGOCard {
                 return;
             }
             if (Context.Channel.Id == 87463676595949568) {
-                await ReplyAsync("Command is disabled in this channel. Please use <#171483830845177857>");
+                await ReplyAsync("Command is disabled in this channel. Please use <#242447505508270081>");
                 return;
             }
             var cards = YGOCardAPIClient.Cards.FindCards(cardName.ToLower());
+            if (!cards.Any()) {
+                await ReplyAsync($"No cards that contain the substring {cardName} were found.");
+                return;
+            }
             var listOfCards = string.Join(", ", cards.Select(x => x.Name));
             if (listOfCards.Length > 2000) {
                 var splitList = Enumerable.Range(0, listOfCards.Length / 1900).
