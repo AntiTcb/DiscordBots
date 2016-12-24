a6703cd227deb3083bf0c76696073f7bcd4c94c0
AntiTcb <tcbskater@hotmail.com>
2016-12-24 11:47:06 -0500

    Case insensitive command name comparer

---
+++ src/BCL/Comparers/CommandNameComparer.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+
+namespace BCL.Comparers
+{
+    public class CommandNameComparer : IEqualityComparer<CommandInfo>
+    {
+        #region Public Methods
+
+        public bool Equals(CommandInfo x, CommandInfo y) => x.Name.Equals(y.Name, StringComparison.OrdinalIgnoreCase);
+
+        public int GetHashCode(CommandInfo obj) => obj.Name.ToLowerInvariant().GetHashCode();
+
+        #endregion Public Methods
+    }
+}
