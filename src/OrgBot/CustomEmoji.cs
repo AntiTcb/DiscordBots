c36e18315ae2a0ce99e1e7426cb224ff732fe0eb
AntiTcb <tcbskater@hotmail.com>
2016-12-24 11:59:42 -0500

    Add custom emoji

---
+++ src/OrgBot/CustomEmoji.cs
@@ -0,0 +1,12 @@
+﻿namespace OrgBot
+{
+    public struct CustomEmoji
+    {
+        #region Public Fields + Properties
+
+        public static string LeftScale = "<:leftscale:251264662572761089>";
+        public static string RightScale = "<:rightscale:251264701730652161>";
+
+        #endregion Public Fields + Properties
+    }
+}
---
+++ src/WiseOldBot/CustomEmoji.cs
@@ -0,0 +1,11 @@
+﻿namespace WiseOldBot
+{
+    public struct CustomEmoji
+    {
+        #region Public Fields + Properties
+
+        public static string Gold = "<:gold:249879488190480384>";
+
+        #endregion Public Fields + Properties
+    }
+}
