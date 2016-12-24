8654b077e6f874ba4f28826d15580192fa41cc9e
AntiTcb <tcbskater@hotmail.com>
2016-12-24 11:52:29 -0500

    Added preconditions, removed RequireOwner due to being baseline.

---
+++ src/BCL/Preconditions/RequireRoleAttribute.cs
@@ -0,0 +1,32 @@
+﻿namespace BCL.Preconditions
+{
+    using Discord;
+    using Discord.Commands;
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class RequireRoleAttribute : PreconditionAttribute
+    {
+        ulong[] _roleIds;
+
+        public RequireRoleAttribute(params ulong[] roleIds)
+        {
+            _roleIds = roleIds;
+        }
+
+        public override async Task<PreconditionResult> CheckPermissions(CommandContext context, CommandInfo command, IDependencyMap map)
+        {
+            if (!(context.User is IGuildUser))
+            {
+                return PreconditionResult.FromError("This command may only be run within a guild.");
+            }
+            var guildUser = context.User as IGuildUser;
+            var hasRole = guildUser.RoleIds.Intersect(_roleIds).Any();
+            return await Task.FromResult(hasRole)
+                ? PreconditionResult.FromSuccess()
+                : PreconditionResult.FromError("You do not have a role required for this command.");
+        }
+    }
+}
---
+++ src/BCL/Preconditions/RequireUserAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace BCL.Preconditions
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Discord.Commands;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class RequireUserAttribute : PreconditionAttribute
+    {
+        ulong[] _userIds;
+        public RequireUserAttribute(params ulong[] userIds)
+        {
+            _userIds = userIds;
+        }
+
+        public override async Task<PreconditionResult> CheckPermissions(CommandContext context, CommandInfo command, IDependencyMap map)
+        {
+            var isWhitelistedUser = _userIds.Contains(context.User.Id);
+            return await Task.FromResult(isWhitelistedUser)
+                ? PreconditionResult.FromSuccess()
+                : PreconditionResult.FromError("You are not allowed to run this command.");
+        }
+    }
+}
