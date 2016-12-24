cf9884d8a8df8aaacf6060af7c75e9fdc65808ca
AntiTcb <tcbskater@hotmail.com>
2016-12-24 11:56:02 -0500

    using EnterTypingState

--- src/OrgBot/Modules/YGOCard/YGOCardModule.cs
+++ src/OrgBot/Modules/YGOCard/YGOCardModule.cs
@@ -1,30 +1,22 @@
-﻿#region Header
-
-// Description:
-// 
+﻿// Description:
+//
 // Solution: DiscordBots
 // Project: OrgBot
-// 
+//
 // Created: 10/19/2016 12:23 AM
 // Last Revised: 10/30/2016 3:47 PM
 // Last Revised by: Alex Gravely
 
-#endregion
-
 namespace OrgBot.Modules.YGOCard {
-    #region Using
 
+    using Discord.Commands;
+    using Entities;
     using System.Linq;
     using System.Text.RegularExpressions;
     using System.Threading.Tasks;
-    using Discord.Commands;
-    using Entities;
-
-    #endregion
 
     [Name("YGOrg")]
     public partial class YGOCardModule : ModuleBase {
-        #region Public Methods
 
         [Command("cardupdate"), Alias("cu"), Summary("Forces a card to update its data from the YGOrg Database."), Remarks("cardupdate sangan")]
         public async Task ForceCardUpdateAsync([Summary("Card name, case insensitive"), Remainder] string cardName) {
@@ -64,37 +56,36 @@ namespace OrgBot.Modules.YGOCard {
 
         [Command("listcards"), Summary("Lists all cards that match a substring, ordered by name length."), Remarks("listcards kuriboh")]
         public async Task GetCardsAsync([Summary("Card name, case insensitive.")Remainder] string cardName) {
-            await Context.Channel.TriggerTypingAsync();
-            if (cardName == "") {
-                await ReplyAsync("I need a card name!");
-                return;
-            }
-            if (Regex.Match(cardName, @"^(\W|\D){1,2}$").Success) {
-                await ReplyAsync("I need a longer card name.");
-                return;
-            }
-            if (Context.Channel.Id == 87463676595949568) {
-                await ReplyAsync("Command is disabled in this channel. Please use <#242447505508270081>");
-                return;
-            }
-            var cards = YGOCardAPIClient.Cards.FindCards(cardName.ToLower());
-            if (!cards.Any()) {
-                await ReplyAsync($"No cards that contain the substring {cardName} were found.");
-                return;
-            }
-            var listOfCards = string.Join(", ", cards.Select(x => x.Name));
-            if (listOfCards.Length > 2000) {
-                var splitList = Enumerable.Range(0, listOfCards.Length / 1900).
-                                           Select(i => listOfCards.Substring(i * 1900, 1900));
-                foreach (var list in splitList.Take(3)) {
-                    await ReplyAsync(list);
-                    await Task.Delay(1000);
+            using (Context.Channel.EnterTypingState()) {
+                if (cardName == "") {
+                    await ReplyAsync("I need a card name!");
+                    return;
                 }
-                return;
+                if (Regex.Match(cardName, @"^(\W|\D){1,2}$").Success) {
+                    await ReplyAsync("I need a longer card name.");
+                    return;
+                }
+                if (Context.Channel.Id == 87463676595949568) {
+                    await ReplyAsync("Command is disabled in this channel. Please use <#242447505508270081>");
+                    return;
+                }
+                var cards = YGOCardAPIClient.Cards.FindCards(cardName.ToLower());
+                if (!cards.Any()) {
+                    await ReplyAsync($"No cards that contain the substring {cardName} were found.");
+                    return;
+                }
+                var listOfCards = string.Join(", ", cards.Select(x => x.Name));
+                if (listOfCards.Length > 2000) {
+                    var splitList = Enumerable.Range(0, listOfCards.Length / 1900).
+                                               Select(i => listOfCards.Substring(i * 1900, 1900));
+                    foreach (var list in splitList.Take(3)) {
+                        await ReplyAsync(list);
+                        await Task.Delay(1000);
+                    }
+                    return;
+                }
+                await ReplyAsync(listOfCards);
             }
-            await ReplyAsync(listOfCards);
         }
-
-        #endregion Public Methods
     }
 }
