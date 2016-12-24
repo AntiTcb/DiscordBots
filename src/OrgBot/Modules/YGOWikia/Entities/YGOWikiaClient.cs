acc64513ad990018ab40aa564f65eef66e4214ef
AntiTcb <tcbskater@hotmail.com>
2016-12-24 11:59:04 -0500

    Bugfixes

--- src/OrgBot/Modules/YGOWikia/Entities/YGOWikiaClient.cs
+++ src/OrgBot/Modules/YGOWikia/Entities/YGOWikiaClient.cs
@@ -1,6 +1,4 @@
-﻿#region Header
-
-// Description:
+﻿// Description:
 //
 // Solution: DiscordBots
 // Project: OrgBot
@@ -9,33 +7,17 @@
 // Last Revised: 11/04/2016 12:47 AM
 // Last Revised by: Alex Gravely
 
-#endregion Header
-
 namespace OrgBot.Modules.YGOWikia.Entities {
-
-    #region Using
 
     using AngleSharp;
     using RestEase;
     using System;
-    using System.Net;
     using System.Linq;
-    using System.Threading.Tasks;
+    using System.Net;
     using System.Text.RegularExpressions;
-
-    #endregion Using
+    using System.Threading.Tasks;
 
     public static class YGOWikiaClient {
-
-        #region Private Fields + Properties
-
-        const string BASE_URI = "http://yugioh.wikia.com/api/v1";
-        static readonly IYGOWikiaAPI API = RestClient.For<IYGOWikiaAPI>(BASE_URI);
-        static readonly Regex URL_EXCLUSION_REGEX = new Regex(@"\((anime|BAM)\)", RegexOptions.IgnoreCase);
-
-        #endregion Private Fields + Properties
-
-        #region Public Methods
 
         public static async Task<YGOWikiaCard> GetCardAsync(string cardName) {
             try {
@@ -47,9 +29,9 @@ namespace OrgBot.Modules.YGOWikia.Entities {
             }
         }
 
-        #endregion Public Methods
-
-        #region Private Methods
+        const string BASE_URI = "http://yugioh.wikia.com/api/v1";
+        static readonly IYGOWikiaAPI API = RestClient.For<IYGOWikiaAPI>(BASE_URI);
+        static readonly Regex URL_EXCLUSION_REGEX = new Regex(@"\((anime|BAM)\)", RegexOptions.IgnoreCase);
 
         static async Task<YGOWikiaCard> ParsePageHTMLAsync(string url) {
             if (url == null) {
@@ -62,11 +44,10 @@ namespace OrgBot.Modules.YGOWikia.Entities {
                                  x =>
                                      x.TextContent.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).
                                        ElementAtOrDefault(1));
-            var ceffect = Regex.Replace(doc.QuerySelectorAll(".navbox-list").Select(x => x.InnerHtml).ElementAtOrDefault(0), "<br>", "\n");
-            var cardeffect = Regex.Replace(Regex.Replace(ceffect, "(<(.*?)>)+?", ""), @"^ *(Monster Effect|Pendulum Effect)\s*\n\s*(?=\w)", "**$1:**\n", RegexOptions.Multiline);
-            return YGOWikiaCard.Parse(cardAttrs, cardeffect, url);
+            var ceffect = Regex.Replace(doc.QuerySelectorAll(".navbox-list").Select(x => x.InnerHtml).ElementAtOrDefault(0) ?? "", "<br>", "\n");
+            var cardeffect = Regex.Replace(Regex.Replace(ceffect, "(<(.*?)>)+?", ""), @"^ *(Monster Effect|Pendulum Effect)\s*\n\s*(\((.*?)\n)?\s*?(?=\w)", "**$1:**\n", RegexOptions.Multiline);
+            var cardimage = doc.QuerySelector(".image")?.Attributes?["href"]?.Value;
+            return YGOWikiaCard.Parse(cardAttrs, cardeffect, url, cardimage);
         }
-
-        #endregion Private Methods
     }
 }
