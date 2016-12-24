a84f13845122b67a55323114a316ef3a6c71db6f
AntiTcb <tcbskater@hotmail.com>
2016-12-24 11:49:47 -0500

    Cleanup, added SayAsync.

--- src/BCL/Modules/Owner/OwnerModule.cs
+++ src/BCL/Modules/Owner/OwnerModule.cs
@@ -1,37 +1,28 @@
-﻿#region Header
-
-// Description:
-// 
+﻿// Description:
+//
 // Solution: DiscordBots
 // Project: BCL
-// 
+//
 // Created: 10/16/2016 5:11 PM
 // Last Revised: 11/04/2016 12:52 AM
 // Last Revised by: Alex Gravely
 
-#endregion
-
 namespace BCL.Modules.Owner {
-    #region Using
 
-    using System;
-    using System.IO;
-    using System.Net.Http;
-    using System.Threading.Tasks;
     using Discord;
     using Discord.API;
     using Discord.Commands;
     using Discord.WebSocket;
     using Extensions;
-    using Preconditions;
-
-    #endregion
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Threading.Tasks;
 
     [Name("Owner"), RequireOwner]
     public partial class OwnerModule : ModuleBase {
-        #region Public Methods    
 
-        [Command("echo"), Summary("echos the user input"), Remarks("echo potato")]
+        [Command("echo"), Summary("Echos the user input."), Remarks("echo potato")]
         public async Task EchoAsync([Remainder, Summary("Text to echo")] string text) {
             await ReplyAsync(text).ConfigureAwait(false);
         }
@@ -43,8 +34,21 @@ namespace BCL.Modules.Owner {
             await Task.Delay(1500).ConfigureAwait(false);
         }
 
+        [Command("say"), Summary("Echos user input into the specified channel"), Remarks("say #general potato")]
+        public async Task SayAsync([Summary("Target channel")] ITextChannel channel, [Remainder, Summary("Text to echo")] string text) {
+            (await Context.Client.GetChannelAsync(channel.Id) as SocketTextChannel)?.SendMessageAsync(text);
+        }
+
+        //[Command("getinvite"), Summary("Makes an invite to the specified guild"), Remarks("getinvite 123456")]
+        //public async Task GetInviteAsync([Summary("Target guild")]IGuild guild)
+        //{
+        //    var channel = await Context.Client.GetChannelAsync(guild.DefaultChannelId);
+        //    var invite = await (channel as SocketGuildChannel).CreateInviteAsync();
+        //    await ReplyAsync(invite.Url);
+        //}
+
         [Command("set"), RequireContext(ContextType.Guild), Summary("sets various bot properties"), Remarks("set nick FooBar")]
-        public async Task SetAsync([Summary("Property to change")]UserProperty prop, 
+        public async Task SetAsync([Summary("Property to change")]UserProperty prop,
             [Summary("Value to change the property to"), Remainder] string value) {
             switch (prop) {
                 case UserProperty.User:
@@ -68,8 +72,7 @@ namespace BCL.Modules.Owner {
                         await client.DownloadAsync(q, q.LocalPath.Replace("/", "")).ConfigureAwait(false);
                         using (var imagestream = new FileStream(q.LocalPath.Replace("/", ""), FileMode.Open)) {
                             await
-                                Context.Client.CurrentUser.ModifyAsync(x => x.Avatar = new Image(imagestream)).
-                                        ConfigureAwait(false);
+                                Context.Client.CurrentUser.ModifyAsync(x => x.Avatar = new Image(imagestream)).ConfigureAwait(false);
                         }
                         File.Delete(q.LocalPath.Replace("/", ""));
                     }
@@ -82,7 +85,7 @@ namespace BCL.Modules.Owner {
                 case UserProperty.Status:
                     var newStatus = Enum.Parse(typeof(UserStatus), value);
                     await
-                        (Context.Client as DiscordSocketClient).SetStatus((UserStatus) newStatus).ConfigureAwait(false);
+                        (Context.Client as DiscordSocketClient).SetStatus((UserStatus)newStatus).ConfigureAwait(false);
                     break;
 
                 default:
@@ -91,7 +94,5 @@ namespace BCL.Modules.Owner {
             }
             await ReplyAsync(":thumbsup:").ConfigureAwait(false);
         }
-
-        #endregion Public Methods
     }
 }
