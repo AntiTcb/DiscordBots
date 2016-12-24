e3299d30a97226b822bd49ddee9be586f09bdc18
AntiTcb <tcbskater@hotmail.com>
2016-12-24 12:00:44 -0500

    Minor fixes

--- src/WiseOldBot/Modules/GETracker/Entites/ItemMap.cs
+++ src/WiseOldBot/Modules/GETracker/Entites/ItemMap.cs
@@ -1,42 +1,33 @@
-﻿#region Header
-
-// Description:
-// 
+﻿// Description:
+//
 // Solution: DiscordBots
 // Project: WiseOldBot
-// 
+//
 // Created: 09/20/2016 12:17 PM
 // Last Revised: 09/20/2016 9:42 PM
 // Last Revised by: Alex Gravely
 
-#endregion
-
 namespace WiseOldBot.Modules.GETracker.Entities {
-    #region Using
 
     using System.Collections.Generic;
     using System.Linq;
 
-    #endregion 
-
     public class ItemMap : Dictionary<string, List<GETrackerItem>> {
-        #region Public Constructors
 
         public ItemMap() { }
 
         public ItemMap(IDictionary<string, List<GETrackerItem>> dict) : base(dict) { }
 
-        internal IEnumerable<GETrackerItem> FindItems(string partialKey)
-            => this.Where(x => x.Key.Contains(partialKey.ToLower())).SelectMany(kvp => kvp.Value);
-
-        internal IEnumerable<GETrackerItem> FindItemOrItems(string itemName) {
+        public IEnumerable<GETrackerItem> FindItemOrItems(string itemName) {
             itemName = itemName.ToLower();
-            if (ContainsKey(itemName)) {
-                return this[itemName];
+            List<GETrackerItem> returnItems;
+            if (TryGetValue(itemName, out returnItems)) {
+                return returnItems;
             }
             return FindItems(itemName);
         }
 
-        #endregion Public Constructors
+        public IEnumerable<GETrackerItem> FindItems(string itemName)
+                    => this.Where(x => x.Key.Contains(itemName.ToLower())).SelectMany(kvp => kvp.Value);
     }
 }
