e5c118f71c3b140c19e43dad6d74a6ae36cb8aa5
Alex Gravely <tcbskater@hotmail.com>
2019-11-07 13:29:51 -0500

    Start yugipedia code

---
+++ src/OrgBot/Modules/Yugipedia/Entities/YugipediaCard.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrgBot
+{
+    public class YugipediaCard
+    {
+
+    }
+}
---
+++ src/OrgBot/Modules/Yugipedia/YugipediaModule.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace OrgBot
+{
+    public class YugipediaModule : ModuleBase<SocketCommandContext>
+    {
+        public YugipediaService Yugipedia { get; set; }
+
+        [Command("card"), Alias("c"), Summary("Searches Yugipedia and returns card information."), Remarks("card sangan")]
+        public async Task GetCardAsync([Summary("Card name, case insensitive"), Remainder] string cardName)
+        {
+
+        }
+    }
+}
